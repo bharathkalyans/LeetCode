b7eb12d3b991354da88d51c0d295a202b90be160
Bharath Kalyan S <sbharath35@gmail.com>
2022-03-18 19:08:45 +0530

    Implemented Longest Prefix for a Word.[TRIE]

--- src/RootSample.java
+++ src/RootSample.java
@@ -2,12 +2,9 @@ public class RootSample {
     public static void main(String[] args) {
         Trie t = new Trie();
 
-        String[] arr = {"zebra", "dog", "duck", "dove"};
+        String[] arr = {"doozebra", "dog", "douck", "dove"};
         for (String w : arr) t.insert(w);
 
-        for (String w : arr) {
-            System.out.println(t.findShortestPrefix(w));
-        }
 
 
     }
--- src/Trie.java
+++ src/Trie.java
@@ -2,7 +2,7 @@ import java.util.HashMap;
 
 class TrieNode {
 
-    private HashMap<Character, TrieNode> map = new HashMap<>();
+    HashMap<Character, TrieNode> map = new HashMap<>();
     boolean flag = false;
     int frequency = 0;
 
@@ -10,25 +10,28 @@ class TrieNode {
     }
 
     boolean containsKey(char c) {
-        return map.containsKey(c);
+        return this.map.containsKey(c);
     }
 
     TrieNode get(char c) {
-        return map.get(c);
+        return this.map.get(c);
     }
 
     void put(char c, TrieNode node) {
-        map.put(c, node);
+        this.map.put(c, node);
     }
 
     boolean isEnd() {
-        return flag;
+        return this.flag;
     }
 
     void setFlag() {
-        flag = true;
+        this.flag = true;
     }
 
+    int getMapSize() {
+        return this.map.size();
+    }
 }
 
 public class Trie {
@@ -81,6 +84,20 @@ public class Trie {
             }
             sb.append(c);
             temp = temp.get(c);
+        }
+
+        return sb.toString();
+    }
+
+    public String findLongestCommonPrefix() {
+        StringBuilder sb = new StringBuilder();
+        TrieNode temp = root;
+
+        while (temp != null && !temp.isEnd() && temp.map.size() == 1) {
+            for (var entry : temp.map.entrySet()) {
+                sb.append(entry.getKey());
+                temp = entry.getValue();
+            }
         }
 
         return sb.toString();
