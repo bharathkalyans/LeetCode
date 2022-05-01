5cb023e5047a0832fd2c095a8c72033b667ba0e6
Bharath Kalyan S <sbharath35@gmail.com>
2022-05-01 10:12:18 +0530

    Leetcode BackSpace Compare

--- src/leetcode/LeetCode.java
+++ src/leetcode/LeetCode.java
@@ -6,7 +6,28 @@ import java.util.*;
 public class LeetCode {
     public static void main(String[] args) {
 
+    }
 
+    //https://leetcode.com/problems/backspace-string-compare/discuss/?currentPage=1&orderBy=most_votes&query=
+    public boolean backspaceCompare(String s, String t) {
+        StringBuilder sb1 = new StringBuilder();
+        StringBuilder sb2 = new StringBuilder();
+
+        for (char c : s.toCharArray()) {
+            if (c == '#') {
+                if (sb1.length() == 0) continue;
+                sb1.deleteCharAt(sb1.length() - 1);
+            } else sb1.append(c);
+        }
+
+        for (char c : t.toCharArray()) {
+            if (c == '#') {
+                if (sb2.length() == 0) continue;
+                sb2.deleteCharAt(sb2.length() - 1);
+            } else sb2.append(c);
+        }
+
+        return sb1.toString().equals(sb2.toString());
     }
 
     public static List<Long> maxSubSetSum(List<Integer> k) {
