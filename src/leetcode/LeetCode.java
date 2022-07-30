c15df6e83b7fc296fc1c3d34870d15f89258e7d5
Bharath Kalyan S <sbharath35@gmail.com>
2022-07-30 11:29:31 +0530

    Word SubSet problem!

--- src/leetcode/LeetCode.java
+++ src/leetcode/LeetCode.java
@@ -5,7 +5,46 @@ import java.util.*;
 
 public class LeetCode {
     public static void main(String[] args) {
+        String[] words1 = new String[]{"amazon", "apple", "facebook", "google", "leetcode"};
+        String[] words2 = new String[]{"e", "o"};
+        System.out.println(Arrays.toString(new LeetCode().wordSubsets(words1, words2).toArray()));
+    }
 
+    //https://leetcode.com/problems/word-subsets/
+    public List<String> wordSubsets(String[] words1, String[] words2) {
+        List<String> result = new ArrayList<>();
+        int[] highestFrequencyOfCharacters = buildHighestFrequency(words2);
+
+        for (String word : words1) {
+            boolean isSubSet = true;
+            int[] currentFrequency = getFrequencyOfCharacters(word);
+            for (int i = 0; i < 26; i++) {
+                if (currentFrequency[i] < highestFrequencyOfCharacters[i]) {
+                    isSubSet = false;
+                    break;
+                }
+            }
+            if (isSubSet) result.add(word);
+        }
+
+        return result;
+    }
+
+    private int[] buildHighestFrequency(String[] words2) {
+        int[] frequencyOfCharacters = new int[26];
+        for (String word : words2) {
+            int[] currentFrequencyOfWord = getFrequencyOfCharacters(word);
+            for (int i = 0; i < 26; i++)
+                frequencyOfCharacters[i] = Math.max(
+                        frequencyOfCharacters[i], currentFrequencyOfWord[i]);
+        }
+        return frequencyOfCharacters;
+    }
+
+    private int[] getFrequencyOfCharacters(String word1) {
+        int[] frequencyOfCharacters = new int[26];
+        for (char c : word1.toCharArray()) frequencyOfCharacters[c - 'a']++;
+        return frequencyOfCharacters;
     }
 
     public List<String> findAndReplacePattern(String[] words, String pattern) {
